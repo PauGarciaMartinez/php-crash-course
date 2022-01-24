d12b9cd6cf5dfea478b78443fe86ffe5cc42a455
Pau Garcia Martínez <etpau@hotmail.es>
2022-01-24 18:59:01 +0100

    Finish strings section·

--- 04_strings.php
+++ 04_strings.php
@@ -1,13 +1,40 @@
 <?php
 
-// Create simple string
-
-// String concatenation
+// Create simple string and concatenation
+$name = 'Pau';
+$string = 'Hello I am' . $name . ' and I am 24';
+$string2 = "Hello Universe, I am $name";
+echo $string . '<br>';
+echo $string2 . '<br>';
 
 // String functions
+echo "1 - " . strlen($string) . '<br>';
+echo "1 - " . trim($string) . '<br>';
+echo "1 - " . ltrim($string) . '<br>';
+echo "1 - " . rtrim($string) . '<br>';
+echo "1 - " . str_word_count($string) . '<br>';
+echo "1 - " . strrev($string) . '<br>';
+echo "1 - " . strtoupper($string) . '<br>';
+echo "1 - " . strtolower($string) . '<br>';
+echo "1 - " . ucfirst($string) . '<br>';
+echo "1 - " . lcfirst($string) . '<br>';
+echo "1 - " . ucwords($string) . '<br>';
+echo "1 - " . strpos($string, 'world') . '<br>';
+echo "1 - " . stripos($string, 'world') . '<br>';
+echo "1 - " . substr($string, 2) . '<br>';
+echo "1 - " . str_replace('world', 'moon', $string) . '<br>';
+echo "1 - " . str_ireplace('world', 'moon', $string) . '<br>';
 
 // Multiline text and line breaks
-
-// Multiline text and reserve html tags
+$longText = "
+  Hello, 
+  my name is <b>Pau</b>
+  I like <b>pizza</b>
+";
+echo $longText . '<br>';
+echo nl2br($longText) . '<br>';
+echo htmlentities($longText) . '<br>';
+echo nl2br(htmlentities($longText)) . '<br>';
+echo nl2br(htmlentities($longText)) . '<br>';
 
 // https://www.php.net/manual/en/ref.strings.php
