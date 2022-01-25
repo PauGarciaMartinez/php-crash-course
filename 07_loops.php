8569472dcb734eb32431b81dd5dfcc86d2acb218
Pau Garcia Martínez <etpau@hotmail.es>
2022-01-26 00:20:22 +0100

    Finish loops section

--- 07_loops.php
+++ 07_loops.php
@@ -1,13 +1,43 @@
 <?php
 
-// while
+// While
+$counter = 0;
 
-// Loop with $counter
+while ($counter < 10) {
+  echo $counter;
+  $counter++;
+}
 
 // do - while
+do {
+  echo $counter;
+  $counter++;
+} while ($counter < 10);
 
 // for
+for ($i = 0; $i < 10; $i++) {
+  echo $i;
+}
 
 // foreach
+$fruits = ['Banana', 'Melon', 'Apple'];
 
-// Iterate Over associative array.
+foreach ($fruits as $i => $fruit) {
+  echo $i.' '.$fruit;
+}
+
+// Iterate Over associative array
+$person = [
+  'name' => 'Pau',
+  'surname' => 'Garcia',
+  'age' => 24,
+  'hobbies' => ['Sport', 'Coding', 'Videogames']
+];
+
+foreach ($person as $key => $value) {
+  if (is_array($value)) {
+    echo $key.' '.implode(',', $value);
+  } else {
+    echo $key.' '.$value;
+  }
+}
