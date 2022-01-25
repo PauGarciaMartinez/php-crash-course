91a03f42eeef61a9edbacc6d2fa279b5202ac898
Pau Garcia Martínez <etpau@hotmail.es>
2022-01-25 23:59:50 +0100

    Start conditional exercise

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -19,8 +19,18 @@ if ($age > 20) {
 }
 
 // Difference between == and ===
+if ($age == 20) {
+  echo '1';
+}
+
+if ($age === '20') {
+  echo '2';
+}
 
 // if AND
+if ($age > 20 && $salary === 300000) {
+  echo 'Good money man';
+}
 
 // if OR
 
