dbd14699b818b4e7c725b9aee47897aa0064db5f
Pau Garcia Martínez <etpau@hotmail.es>
2022-01-27 16:09:53 +0100

    Create better version of product CRUD

---
+++ 14_product_crud/02_better/public/products/update.php
@@ -0,0 +1,60 @@
+<?php
+
+/** @var $pdo \PDO */
+require_once "../../database.php";
+require_once "../../functions.php";
+
+$id = $_GET['id'] ?? null;
+
+if (!$id) header('Location: index.php');
+
+$statement = $pdo->prepare("SELECT * FROM products WHERE id = :id");
+$statement->bindValue(':id', $id);
+$statement->execute();
+$statement->fetch(PDO::FETCH_ASSOC);
+
+$errors = [];
+
+$title = $product['title'];
+$description = $product['description'];
+$price = $product['price'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  require_once "../../validate_product.php";
+
+  if (empty($errors)) {
+    $statement = $pdo->prepare("
+      UPDATE products 
+        SET 
+          title = :title, 
+          image = :image, 
+          description = :description, 
+          price = :price
+        WHERE 
+          id = :id
+    ");
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':image', $imagePath);
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+
+    header('Location: index.php');
+  }
+}
+?>
+
+<?php include_once "../../views/partials/header.php" ?>
+
+    <p>
+      <a href="index.php" class="btn btn-secondary">Go back to products</a>
+    </p>
+
+    <h1>Update product <?php echo $product['title'] ?></h1>
+    
+    <?php include_once "../../views/products/form.php"?>
+    
+  </body>
+</html>
---
+++ 14_product_crud/02_better/views/products/form.php
@@ -0,0 +1,31 @@
+<?php if (!empty($errors)): ?>
+  <div class="alert-danger">
+  <?php foreach ($errors as $i => $error): ?>
+    <div><?php echo $error ?></div>
+  <?php endforeach; ?>
+  </div>
+<?php endif; ?>
+
+<form action="" method="POST" enctype="multipart/form-data">
+  <?php if ($product['image']): ?>
+    <img src="<?php echo $product['image'] ?>" class="update-img">
+  <?php endif; ?>
+  <div class="mb-3">
+    <label>Product Image</label>
+    <br>
+    <input type="file" name="image">
+  </div>
+  <div class="mb-3">
+    <label>Product Title</label>
+    <input type="text" class="form-control" name="title" value="<?php echo $title ?>">
+  </div>
+  <div class="mb-3">
+    <label>Product Description</label>
+    <textarea type="text" class="form-control" name="description" value="<?php echo $description ?>">
+  </div>
+  <div class="mb-3">
+    <label>Product Price</label>
+    <input type="number" step=".01" class="form-control" name="price" value="<?php echo $price ?>">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
