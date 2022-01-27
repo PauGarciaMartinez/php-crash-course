3be7adb3e26c5d392357ced209d6eff58f0a5fd0
Pau Garcia Martínez <etpau@hotmail.es>
2022-01-27 17:01:19 +0100

    Start MVC CRUD application

---
+++ 14_product_crud/03_good/controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app;
+
+class ProductController {
+  public function index() {
+    echo "Index page";
+  }
+
+  public function create() {
+    echo "Create page";
+  }
+
+  public function update() {
+    echo "Update page";
+  }
+
+  public function delete() {
+    echo "Delete page";
+  }
+}
